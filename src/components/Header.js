9327e17a1715f4deef6a14e2a5d5ef3c1729932e
Dee Kay <dk@email.com>
2022-10-30 22:21:57 +0000

    modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ function Header() {
         <div class="container">
             <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
                 <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-                    <img class="bi me-2" width="40" height="40" src="https://clipground.com/images/jordan-crying-meme-clipart-3.png" alt="logo">
+                    <img class="bi me-2" width="40" height="40" src="https://clipground.com/images/jordan-crying-meme-clipart-3.png" alt="logo"></img>
                     <span class="fs-4">Meme Generator</span>
                 </a>
 
