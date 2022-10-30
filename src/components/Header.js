ee8e2120fd3c0089bc94706fdf094b69974301d0
Dee Kay <dk@email.com>
2022-10-30 21:04:37 +0000

    initial start, header created

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+function Header() {
+    return (
+        <div class="container">
+            <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+                <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                    <img class="bi me-2" width="40" height="40" src="https://clipground.com/images/jordan-crying-meme-clipart-3.png" alt="logo">
+                    <span class="fs-4">Meme Generator</span>
+                </a>
+
+                <h4>where memes come to life</h4>
+            </header>
+        </div>
+
+    );
+}
+
+export default Header;
