c3c1abbab0bc2a90464b126c7d6cdb6044f9e5c3
Dee Kay <dk@email.com>
2022-12-24 18:54:15 +0000

    removed dependents on memesData.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import memesData from "../memesData.js"
+//import memesData from "../memesData.js" --not used now
 import React from "react"
 import './Form.css';
 
@@ -6,47 +6,47 @@ function Form() {
     const [meme, setMeme] = React.useState({
         topText: "",
         bottomText: "",
-        randomImage: "http://i.imgflip.com/1bij.jpg" 
+        randomImage: "http://i.imgflip.com/1bij.jpg"
     })
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
-    
-    
-     function getMemeImage() {
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
-        setMeme(prevMeme => ({
-            ...prevMeme,
-            randomImage: url
-        }))
-        
+    const [allMemes, setAllMemes] = React.useState([])
+
+    React.useEffect(() => {
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemes(data.data.memes)
+        }
+        getMemes()
+    }, [])
+
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
+        setMeme(prevMeme => ({...prevMeme, randomImage: url}))
     }
 
     function handleChange(event) {
-        const {name, value} = event.target
-        setMeme(prevMeme => ({
-            ...prevMeme,
-            [name]: value
-        }))
+        const { name, value } = event.target
+        setMeme(prevMeme => ({...prevMeme, [name]: value}))
     }
 
     return (
         <main>
-        <div class="container">
-            <form class="needs-validation" novalidate>
-                <div class="row g-3">
-                    <div class="col-sm-6">
-                        <input type="text" className="form-control" name="topText" value={meme.topText} onChange={handleChange} placeholder="Top Text"></input>
+            <div class="container">
+                <form class="needs-validation" novalidate>
+                    <div class="row g-3">
+                        <div class="col-sm-6">
+                            <input type="text" className="form-control" name="topText" value={meme.topText} onChange={handleChange} placeholder="Top Text"></input>
+                        </div>
+                        <div class="col-sm-6">
+                            <input type="text" className="form-control" name="bottomText" value={meme.bottomText} onChange={handleChange} placeholder="Bottom Text"></input>
+                        </div>
                     </div>
-                    <div class="col-sm-6">
-                        <input type="text" className="form-control" name="bottomText" value={meme.bottomText} onChange={handleChange} placeholder="Bottom Text"></input>
-                    </div>
-                </div>
-                <button class="w-100 btn btn-primary btn-lg mt-3" onClick={getMemeImage}>Get Meme</button>
-            </form>
-        </div>
-        <div className="meme">
-                <img src={meme.randomImage} className="meme--image" />
+                    <button class="w-100 btn btn-primary btn-lg mt-3" onClick={getMemeImage}>Get Meme</button>
+                </form>
+            </div>
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" alt="" />
                 <h2 className="meme--text top">{meme.topText}</h2>
                 <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
